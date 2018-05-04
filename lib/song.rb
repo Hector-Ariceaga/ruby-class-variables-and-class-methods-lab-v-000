420ccfb05e46965d2f0bd7abcb8db644da05caab
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-04 00:27:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,6 +41,14 @@ class Song
   end
   
   def self.artist_count
-  
+    artist_count = {}
+    
+    @@artists.map do |artist|
+      if artist_count.include?(arist)
+        artist_count[artist] += 1
+      else
+        artist_count[arist] = 1
+      end
+    end
   end
 end
