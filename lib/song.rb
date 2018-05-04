71c3bea3ac8658e18cfd34678d62ee1fc41a7be3
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-04 00:28:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
   def self.artist_count
     artist_count = {}
     
-    @@artists.collect do |artist|
+    @@artists.select do |artist|
       if artist_count.include?(artist)
         artist_count[artist] += 1
       else
